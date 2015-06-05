9ae74648a80456fb4f7776871d829221a0a25829
Jhonathan Wyterlin <jhonathan_wyterlin@yahoo.com.br>
2015-06-04 23:28:39 -0300

    Created FundModel

---
+++ Graduation Funds/Graduation Funds/Model/Model Subclasses/FundModel.h
@@ -0,0 +1,16 @@
+//
+//  FundModel.h
+//  Graduation Funds
+//
+//  Created by Jhonathan Wyterlin on 04/06/15.
+//  Copyright (c) 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FundModel : NSObject
+
+@property(nonatomic,strong) NSNumber *identifier;
+@property(nonatomic,strong) NSString *name;
+
+@end
---
+++ Graduation Funds/Graduation Funds/Model/Model Subclasses/FundModel.m
@@ -0,0 +1,13 @@
+//
+//  FundModel.m
+//  Graduation Funds
+//
+//  Created by Jhonathan Wyterlin on 04/06/15.
+//  Copyright (c) 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import "FundModel.h"
+
+@implementation FundModel
+
+@end
