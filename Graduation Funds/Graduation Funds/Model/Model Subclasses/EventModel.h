42fa031f6fdc406f8f56b0090d58e75dcb20328d
Jhonathan Wyterlin <jhonathan_wyterlin@yahoo.com.br>
2015-06-04 23:12:04 -0300

    Created EventModel

---
+++ Graduation Funds/Graduation Funds/Model/Model Subclasses/EventModel.h
@@ -0,0 +1,17 @@
+//
+//  EventModel.h
+//  Graduation Funds
+//
+//  Created by Jhonathan Wyterlin on 04/06/15.
+//  Copyright (c) 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EventModel : NSObject
+
+@property(nonatomic,strong) NSNumber *identifier;
+@property(nonatomic,strong) NSString *name;
+@property(nonatomic,strong) NSDate *date;
+
+@end
