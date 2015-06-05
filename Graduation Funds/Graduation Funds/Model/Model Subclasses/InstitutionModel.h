8bdd04e9bcc9edeced43db97e38db3f9bfec815b
Jhonathan Wyterlin <jhonathan_wyterlin@yahoo.com.br>
2015-06-04 23:30:50 -0300

    Created InstitutionModel

---
+++ Graduation Funds/Graduation Funds/Model/Model Subclasses/InstitutionModel.h
@@ -0,0 +1,16 @@
+//
+//  InstitutionModel.h
+//  Graduation Funds
+//
+//  Created by Jhonathan Wyterlin on 04/06/15.
+//  Copyright (c) 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface InstitutionModel : NSObject
+
+@property(nonatomic,strong) NSNumber *identifier;
+@property(nonatomic,strong) NSString *name;
+
+@end
