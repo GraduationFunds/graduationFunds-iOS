bb68841d8f887e462069a64f6f69cb8fae3bbcc4
Jhonathan Wyterlin <jhonathan_wyterlin@yahoo.com.br>
2015-06-04 23:20:12 -0300

    Created StudentModel

---
+++ Graduation Funds/Graduation Funds/Model/Model Subclasses/StudentModel.h
@@ -0,0 +1,19 @@
+//
+//  StudentModel.h
+//  Graduation Funds
+//
+//  Created by Jhonathan Wyterlin on 04/06/15.
+//  Copyright (c) 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StudentModel : NSObject
+
+@property(nonatomic,strong) NSNumber *identifier;
+@property(nonatomic,strong) NSString *name;
+@property(nonatomic,strong) NSString *email;
+@property(nonatomic,strong) NSNumber *phone;
+@property(nonatomic,strong) NSNumber *cellPhone;
+
+@end
