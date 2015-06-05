cc28ed3bd9cdb34746a7cb7d206046423f7b7d19
Jhonathan Wyterlin <jhonathan_wyterlin@yahoo.com.br>
2015-06-04 23:33:00 -0300

    Created CourseModel

---
+++ Graduation Funds/Graduation Funds/Model/Model Subclasses/CourseModel.h
@@ -0,0 +1,16 @@
+//
+//  CourseModel.h
+//  Graduation Funds
+//
+//  Created by Jhonathan Wyterlin on 04/06/15.
+//  Copyright (c) 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CourseModel : NSObject
+
+@property(nonatomic,strong) NSNumber *identifier;
+@property(nonatomic,strong) NSString *name;
+
+@end
---
+++ Graduation Funds/Graduation Funds/Model/Model Subclasses/CourseModel.m
@@ -0,0 +1,13 @@
+//
+//  CourseModel.m
+//  Graduation Funds
+//
+//  Created by Jhonathan Wyterlin on 04/06/15.
+//  Copyright (c) 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import "CourseModel.h"
+
+@implementation CourseModel
+
+@end
